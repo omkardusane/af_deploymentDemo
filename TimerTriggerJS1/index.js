4256ee99dd60e0a70c9234d68d19d1a124db59a9
Omkar Dusane <omkardusane@gmail.com>
2017-03-15 02:52:45 +0530

    sample
    sample

--- TimerTriggerJS1/index.js
+++ TimerTriggerJS1/index.js
@@ -1,11 +1,4 @@
 module.exports = function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
-    
-    if(myTimer.isPastDue)
-    {
-        context.log('hello world , You are watching Omkar\'s web series on azure functions');
-    }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
-    
+    context.log('hello world , You are watching Omkar\'s web series on azure functions');
     context.done();
 };
